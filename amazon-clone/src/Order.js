d416dc99fc0fdcee3390ecf0d8901a17ffd1817a
suresh shrestha <surace7523@gmail.com>
2021-06-25 02:23:04 -0500

    strip_done

---
+++ amazon-clone/src/Order.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Order() {
+    return (
+        <div>
+           <h3>orderbook</h3> 
+        </div>
+    )
+}
+
+export default Order
--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: "AIzaSyD5sG31rtC4lcmrgirOj55QqGnKMYz9sCw",
   authDomain: "clone-b2d1e.firebaseapp.com",
@@ -9,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "clone-b2d1e.appspot.com",
   messagingSenderId: "1015087346049",
   appId: "1:1015087346049:web:922a190e04511745263880",
-  measurementId: "G-ME2BGPW3RM",
+  measurementId: "G-ME2BGPW3RM"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
